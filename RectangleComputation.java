1a168a6f9c49ba7a5f99edf1dd544b48872eb4dd
Sachin <sachin639651@gmail.com>
2023-01-11 14:18:32 +0530

    changed height and width

--- RectangleComputation.java
+++ RectangleComputation.java
@@ -1,53 +1,53 @@
 
 class RectangleComputation {
-  
+
   double height;
-  
+
   double radius;
 
   public RectangleComputation() {
-   
-     height = 5.3;
-	 radius=3.4;
+
+    height = 10.4;
+    radius = 8.4;
   }
 
-  public RectangleComputation(double height , double radius) {
-   
+  public RectangleComputation(double height, double radius) {
+
     this.height = height;
-    this.radius=radius; 	
+    this.radius = radius;
   }
 
- /*public double length(){
-return length ;
+  /*
+   * public double length(){
+   * return length ;
+   * 
+   * }
+   * public double width(){
+   * return width ;
+   * 
+   * }
+   * public double getrectanglearea(){
+   * return length*width;
+   * 
+   * }
+   * public double getrectanglecircumference(){
+   * return 2*(length+width);
+   * 
+   * }
+   */
 
-}
- public double width(){
-return width ;
+  public double alldataofcyclender() {
+    double SurfaceArea = 2 * 3.14 * height;
+    double BaseArea = 3.14 * radius * radius;
+    return SurfaceArea;
 
-}
-public double getrectanglearea(){
-return  length*width;
-
-}
-public double getrectanglecircumference(){
-return  2*(length+width);
-
-}*/
-
-public double alldataofcyclender(){
-double SurfaceArea=2*3.14*height;
-	double BaseArea=3.14*radius*radius;
-	return SurfaceArea;
-	 
-}
+  }
 
   public static void main(String[] args) {
-	  
-	  
-	  
-    RectangleComputation st = new RectangleComputation(15.3,10.4);
- 
-    System.out.println("SurfaceArea of the cyclender is :"+st.alldataofcyclender());
+
+    RectangleComputation st = new RectangleComputation(15.3, 10.4);
+
+    System.out.println("SurfaceArea of the cyclender is :" + st.alldataofcyclender());
 
   }
 }
