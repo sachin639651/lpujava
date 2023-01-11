eb6e8a5a5e75786b880061f9ceabde0dbb63315f
Sachin <sachin639651@gmail.com>
2023-01-11 22:05:28 +0530

    sum of number from 9 to 899

---
+++ RunningNumberSum.java
@@ -0,0 +1,15 @@
+public class RunningNumberSum { 
+ public static void main(String[] args) {
+ final int LOWERBOUND = 9;  
+ final int UPPERBOUND = 899;  
+ int sum = 0;  
+ int number = LOWERBOUND;
+ while (number <= UPPERBOUND) {
+ sum = sum + number;  
+ ++number;  
+ }
+ 
+ System.out.println("The sum from " + LOWERBOUND + " to " +
+UPPERBOUND + " is " + sum);
+ }
+}
