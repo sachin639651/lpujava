83bc7f2f3d03ce457922fba44b63d69901e58cfd
Sachin <sachin639651@gmail.com>
2023-01-12 16:54:16 +0530

    code of date 12 jan

---
+++ gratestint.java
@@ -0,0 +1,15 @@
+public class gratestinteger{
+
+
+public static void main(String[] args){
+  
+   byte x = 127;
+                     x++;
+                     x++;
+                     System.out.print(x);
+
+}
+
+
+
+}
