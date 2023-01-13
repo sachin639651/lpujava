1683b46481261551e378bf21b4b226a612ca4245
Sachin <sachin639651@gmail.com>
2023-01-13 22:09:55 +0530

    some changes in calculator and gratest integer file

--- gratestint.java
+++ gratestint.java
@@ -1,15 +1,12 @@
-public class gratestinteger{
+class gratestinteger {
 
+   public static void main(String[] args) {
 
-public static void main(String[] args){
-  
-   byte x = 127;
-                     x++;
-                     x++;
-                     System.out.print(x);
+      byte x = 127;
+      x++;
+      x++;
+      System.out.print(x);
 
-}
-
-
+   }
 
 }
