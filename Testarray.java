bc9942a0683fabdad8cc500a582cd78a2a41bd0d
Sachin <sachin639651@gmail.com>
2023-01-14 09:56:22 +0530

    Testarray file added

---
+++ Testarray.java
@@ -0,0 +1,19 @@
+class Testarray{  
+public static void main(String args[]){  
+int a[]=new int[5]; 
+a[0]=1; 
+a[1]=2;  
+a[2]=7;  
+a[3]=4;  
+a[4]=5;  
+ 
+ int sum=0;
+ 
+ 
+for(int i=0;i<a.length;i++) {
+	
+	sum+=a[i];
+	
+}
+System.out.println(sum);  
+}}  
