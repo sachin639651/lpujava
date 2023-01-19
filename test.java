128bcc5941feaa54900941b9748c1292ebe6847d
sachin <sachin639651@gmail.com>
2023-01-20 02:06:00 +0530

    modefy test .java from termux

--- test.java
+++ test.java
@@ -2,7 +2,8 @@ class test{
 public static void main(String args[]){
 
 
-System.out.println("this programme is written in termmux");
+
+System.out.println("this programme is written in termmux and this file again changed");
 }
 
 
